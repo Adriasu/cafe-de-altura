ffe8e2c7fbe7d0bfd6e9d08c0199612d974e9c03
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-07-31 23:55:52 +0200

    terminadas cards y borrar usuario

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -2,7 +2,11 @@ import { MailCheckIcon } from "lucide-react";
 import React from "react";
 import Buttons from "./Buttons";
 
-const UserCard = ({ userCard }) => {
+const UserCard = ({ userCard, funDelete }) => {
+  const deleteUser = () => {
+    funDelete(userCard.phone);
+  };
+
   return (
     <div className="p-3 border-[1px] border-[#D1D5DB] bg-white w-[400px] rounded-lg">
       <h1>Nombre: {userCard.name} </h1>
@@ -12,8 +16,12 @@ const UserCard = ({ userCard }) => {
       <p>Género: {userCard.gender}</p>
       <p>Cuenta: {userCard.account}</p>
       <p>Recibir publicidad: {userCard.publicity ? "si" : "no"}</p>
-      <button className="px-2 py-1 bg-[#2A5B45] rounded text-white">borrar</button>
-      
+      <button
+        onClick={deleteUser}
+        className="px-2 py-1 bg-[#2A5B45] rounded text-white mt-1"
+      >
+        borrar
+      </button>
     </div>
   );
 };
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -6,10 +6,18 @@ import UserCard from "./UserCard";
 const UserList = () => {
   const { dataUsers, setDataUsers } = useContext(FormContext);
 
+  const deletUser = (phoneSelected) => {
+    const deleteByPhone = dataUsers.findIndex((user) => {
+      return user.phone === phoneSelected;
+    });
+    dataUsers.splice(deleteByPhone, 1);
+    setDataUsers([...dataUsers]);
+  };
+
   return (
-    <div className="flex flex-wrap gap-3 justify-center mt-9">
+    <div className="flex flex-wrap gap-3 justify-center m-9">
       {dataUsers.map((user, i) => {
-        return <UserCard key={i} userCard={user} />;
+        return <UserCard key={i} userCard={user} funDelete={deletUser} />;
       })}
     </div>
   );
