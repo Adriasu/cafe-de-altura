cb930081314bc1a8d20733247a089401c5561d44
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-07-31 13:23:52 +0200

    estilo formulario

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -3,41 +3,88 @@ import React from "react";
 
 const UserForm = () => {
   return (
-    <form>
-      <label htmlFor="name">Nombre:</label>
-      <input type="text" name="name" id="name" placeholder="Ingresa tu nombre"/>
+    <div className="bg-[#E3DED7] w-full flex justify-center items-center h-[500px]">
+      <form className="flex flex-col w-[500px] border-[1px] border-[#D1D5DB] rounded-lg gap-6 p-4 text-[#374151] bg-white text-sm">
+        <div className="flex flex-col gap-3  justify-center items-start">
+          <div className="flex flex-col gap-1 w-full">
+            <label htmlFor="name">Nombre:</label>
+            <input
+              className="rounded-md px-2 border-[1px] border-[#D1D5DB] h-[34px]"
+              type="text"
+              name="name"
+              id="name"
+            />
+          </div>
 
-      <label htmlFor="email">E-mail:</label>
-      <input type="text" name="email" id="email" placeholder="Ingresa tu e-mail"/>
+          <div className="flex flex-col gap-1 w-full">
+            <label htmlFor="email">E-mail:</label>
+            <input
+              className="rounded-md px-2 border-[1px] border-[#D1D5DB] h-[34px]"
+              type="text"
+              name="email"
+              id="email"
+            />
+          </div>
 
-      <label htmlFor="age">Edad:</label>
-      <input type="text" name="age" id="age" placeholder="Ingresa tu edad" />
+          <div className="flex flex-col gap-1 w-full">
+            <label htmlFor="age">Edad:</label>
+            <input
+              className="rounded-md px-2 border-[1px] border-[#D1D5DB] h-[34px]"
+              type="text"
+              name="age"
+              id="age"
+            />
+          </div>
+        </div>
 
-      <p>Género:</p>
-      <input type="radio" id="famale" name="gender" value="female" />
-      <label htmlFor="famale">Mujer</label>
-      <input type="radio" id="male" name="gender" value="male" />
-      <label htmlFor="male">Hombre</label>
-      <input type="radio" id="nonbinary" name="gender" value="nonbinary" />
-      <label htmlFor="nonbinary">No binario</label>
-      <input type="radio" id="other" name="gender" value="other" />
-      <label htmlFor="other">Otros / Prefiero no decirlo</label>
+        <div className="flex flex-col gap-3">
+          <p>Género:</p>
+          <div className="grid grid-cols-2 gap-1">
+            <div className="flex gap-2">
+              <input type="radio" id="famale" name="gender" value="female" />
+              <label htmlFor="famale">Mujer</label>
+            </div>
+            <div className="flex gap-2">
+              <input type="radio" id="male" name="gender" value="male" />
+              <label htmlFor="male">Hombre</label>
+            </div>
+            <div className="flex gap-2">
+              <input
+                type="radio"
+                id="nonbinary"
+                name="gender"
+                value="nonbinary"
+              />
+              <label htmlFor="nonbinary">No binario</label>
+            </div>
+            <div className="flex gap-2">
+              <input type="radio" id="other" name="gender" value="other" />
+              <label htmlFor="other">Otros / Prefiero no decirlo</label>
+            </div>
+          </div>
+        </div>
 
-      <label htmlFor="account">Tipo de cuenta:</label>
-      <select name="account" id="account">
-        <option value="" disabled selected>Escoge una</option>
-        <option value="basic">Básica</option>
-        <option value="premium">Premium</option>
-        <option value="business">Business</option>
-      </select>
+        <div className="flex gap-3">
+          <label htmlFor="account">Tipo de cuenta:</label>
+          <select name="account" id="account">
+            <option value="" disabled selected>
+              Escoge una
+            </option>
+            <option value="basic">Básica</option>
+            <option value="premium">Premium</option>
+            <option value="business">Business</option>
+          </select>
+        </div>
 
-      <label htmlFor="publicity">Acepto recibir publicidad por email:</label>
-      <input type="checkbox" id="publicity" name="publicity" value={true} />
-
-    </form>
+        <div className="flex gap-3">
+          <label htmlFor="publicity">
+            Acepto recibir publicidad por email:
+          </label>
+          <input type="checkbox" id="publicity" name="publicity" value={true} />
+        </div>
+      </form>
+    </div>
   );
 };
 
 export default UserForm;
-
-
