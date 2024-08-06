4d24ad5ecbe92ef97ec7bf5e6394f063bf960c4c
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-08-06 21:19:58 +0200

    Update FeatureSection.jsx

--- src/app/(main)/FeatureSection.jsx
+++ src/app/(main)/FeatureSection.jsx
@@ -1,23 +1,47 @@
-import { ArrowRight } from 'lucide-react'
-import Image from 'next/image'
-import React from 'react'
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 const FeatureSection = () => {
-  return (
-    <div>
-        <div>
-            <div>
-                <h2>Pruébalo en nuestro coffee shop</h2>
-                <p> Visita nuestra cafetería en el centro de la ciudad para probar los
-                granos de café antes de hacer tu pedido y llévate un descuento</p>
-                <div>
-                    <a href="">Cómo llegar <ArrowRight/></a>                    
-                </div>
-            </div>
-            <Image src="/images/imagesFeatureSection.jpg" alt='img Feature' height={390} width={588}/>
-        </div>      
-    </div>
-  )
-}
+  const fetchPrueba = async () => {
+    try {
+      const response = await fetch(
+        "https://cafe-de-altura-lovat.vercel.app/api/products"
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log("error");
+    }
+  };
 
-export default FeatureSection
+  fetchPrueba()
+
+  return (
+    <div className="h-[480px] bg-[#F7F5F3] flex justify-center items-center">
+      <div className="flex min-h-[390px] w-[1200px] justify-between items-center">
+        <div className="flex flex-col gap-4 max-w-[457px] min-h-[116px]">
+          <h2 className="text-[#2A5B45] text-2xl font-medium leading-7">Pruébalo en nuestro coffee shop</h2>
+          <p className="text-gray-900 text-sm font-normal leading-4">
+            Visita nuestra cafetería en el centro de la ciudad para probar los
+            granos de café antes de hacer tu pedido y llévate un descuento
+          </p>
+          <div>
+            <a className="leading-4 font-semibold text-black flex gap-4 items-center text-sm" href="">
+              Cómo llegar <ArrowRight />
+            </a>
+          </div>
+        </div>
+        <Image
+          src="/images/imagesFeatureSection.jpg"
+          alt="img Feature"
+          height={390}
+          width={588}
+          className="rounded-[20px]"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FeatureSection;
