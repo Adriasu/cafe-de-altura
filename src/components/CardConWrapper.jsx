ffe5265625e856e292f263a328980699060b8876
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-08-06 15:18:35 +0200

    agregando imagenes del componente wrapper

--- src/components/CardConWrapper.jsx
+++ src/components/CardConWrapper.jsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
 
-const CardConWrapper = ({textH3, textP}) => {
+const CardConWrapper = ({textH3, textP, imgRoute}) => {
   return (
     <div className='w-[316px] h-[232px] border-[1px] border-[#F7F5F3] flex flex-col gap-4 shadow-[0px_1px_4px_0px_#E3DED7CC] items-center justify-center rounded-lg bg-[#FFFFFF]'>
       <div className='min-w-[64px] min-h-[64px] bg-[#2A5B45] flex justify-center items-center rounded-[20px]'>
-        <p>imagen</p>
+        <Image src={imgRoute} alt='imagen' width={40} height={40}/>
       </div>
       <h3 className='w-[268px] h-6 text-lg font-semibold leading-6 text-center text-[#2B2A2B]'>{textH3}</h3>
       <p className='font-normal text-sm text-center text-gray-900 w-[268px]'>{textP}</p>
--- src/components/CardsConditions.jsx
+++ src/components/CardsConditions.jsx
@@ -3,7 +3,7 @@ import CardConWrapper from "./CardConWrapper";
 
 const CardsConditions = () => {
   return (
-    <div className="min-h-[380px] bg-[#e3bc88] flex flex-col items-center justify-center gap-6">
+    <div className="min-h-[380px] bg-[url('/images/backgroundCards.jpeg')] bg-center bg-no-repeat bg-cover flex flex-col items-center justify-center gap-6">
       <h2 className="w-[350px] h-7 text-white text-2xl leading-7 font-medium">
         Café con las mejores condiciones
       </h2>
@@ -13,18 +13,21 @@ const CardsConditions = () => {
           textP={
             "Tienes cosas más importantes en la cabeza, por eso con nuestra suscripción de café, nunca te quedarás sin tu taza de la mañana."
           }
+          imgRoute={"/images/ClipboardCheck.png"}
         />
         <CardConWrapper
           textH3={"Envío en 24/48h"}
           textP={
             "Pide tu café antes de las 12h y lo recibirás al día siguiente."
           }
+          imgRoute={"/images/Truck.png"}
         />
         <CardConWrapper
           textH3={"Descuentos y beneficios"}
           textP={
             "Cada dos meses, te regalamos una bolsa de un nuevo origen sorpresa, para que lo descubras junto a nosotros."
           }
+          imgRoute={"/images/Gift.png"}
         />
       </div>
     </div>
