81f72db7256420f500d511b2ead02deddc5ffd27
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-08-07 21:10:11 +0200

    arreglado lo de la imagen

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -5,7 +5,7 @@ import Buttons from './Buttons'
 const CardProduct = ({data}) => {
   return (
     <div>
-      <img className='w-[219.39px] h-[219.39px]' src={data.img_url} alt="imagen" />
+      <Image src={data.img_url} alt='imagen' width={219.39} height={219.39} />
       <div>
         <p>{data.brand}</p>
         <p>{data.price}</p>
