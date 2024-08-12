915786516fd69e8c05ecfcb1573c17e84df08ab1
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-08-12 23:13:17 +0200

    start footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
     <div>
-        FOOTER
+        <div>
+            <p>cafedealtura.com</p>
+            <Image src="/images/Vector.png" alt='coffe' width={20.53} height={24.93}/>
+        </div>
     </div>
   )
 }
