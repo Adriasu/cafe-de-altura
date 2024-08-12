8ac20cdb2518c8b93540e6196b2933c1f509e97a
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-08-12 14:20:25 +0200

    terminado FAQ

--- src/app/(main)/FAQWrapperElements.jsx
+++ src/app/(main)/FAQWrapperElements.jsx
@@ -1,23 +1,84 @@
-import { ArrowRight } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const FAQWrapperElements = () => {
   return (
-    <div className='min-h-[444px] bg-[#2A5B45] flex flex-col items-center justify-center gap-6'>
-      <h2 className='text-white font-medium text-2xl leading-7 h-[93px] flex justify-center p-[17px]'>Preguntas frecuentes</h2>
+    <div className="min-h-[444px] bg-[#2A5B45] flex flex-col items-center justify-center gap-6">
+      <h2 className="text-white font-medium text-2xl leading-7 h-[93px] flex justify-center p-[17px] items-end">
+        Preguntas frecuentes
+      </h2>
 
-      <div className='w-[178px] h-[72px]'>
-        <Link href={"/"} className='h-6 items-center gap-4 flex justify-center text-sm font-semibold leading-4 text-white'>
-        Resolvemos tus dudas
-        <ArrowRight className='h-6 w-6'/>
+      <Accordion
+        type="single"
+        collapsible
+        className="w-[668px] flex flex-col gap-4"
+      >
+        <AccordionItem
+          value="item-1"
+          className="flex flex-col items-center bg-white min-h-[72px] p-6 rounded-[10px] gap-4"
+        >
+          <AccordionTrigger className="flex justify-between items-center min-w-[620px] min-h-[24px] text-lg font-semibold leading-6">
+            ¿Cómo hago el pedido?
+          </AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-4">
+          <hr className="w-[620px] border border-solid border-[#E3DED7]" />
+            Selecciona el café que desees probar y completa el proceso de
+            compra. Si lo prefieres, te preguntaremos cada cuánto quieres que te
+            lo mandemos a casa y así nunca te quedarás sin café.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem
+          value="item-2"
+          className="flex flex-col items-center bg-white min-h-[72px] p-6 rounded-[10px] gap-4"
+        >
+          <AccordionTrigger className="flex justify-between items-center min-w-[620px] min-h-[24px] text-lg font-semibold leading-6">
+            ¿Por qué los precios son tan bajos?
+          </AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-4">
+          <hr className="w-[620px] border border-solid border-[#E3DED7]" />
+            Viajamos constantemente para encontrar los mejores granos y a los
+            agricultores más exigentes. Si podemos ofrecerte estos precios es
+            porque tratamos directamente con los productores de café, sin
+            intermediarios. Así obtenemos el mejor precio para ti y la persona
+            que está detrás de los granos de café recibe el mayor beneficio
+            posible.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem
+          value="item-3"
+          className="flex flex-col items-center bg-white min-h-[72px] p-6 rounded-[10px] gap-4"
+        >
+          <AccordionTrigger className="flex justify-between items-center min-w-[620px] min-h-[24px] text-lg font-semibold leading-6">
+            ¿Es posible enviar café a mi oficina?
+          </AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-4">
+          <hr className="w-[620px] border border-solid border-[#E3DED7]" />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            sit saepe dolores corporis tempore, nulla aut sunt delectus libero
+            adipisci, quasi temporibus quis quos! Non nobis laudantium mollitia
+            accusantium hic.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+      <div className="w-[178px] h-[72px]">
+        <Link
+          href={"/"}
+          className="h-6 items-center gap-4 flex justify-center text-sm font-semibold leading-4 text-white"
+        >
+          Resolvemos tus dudas
+          <ArrowRight className="h-6 w-6" />
         </Link>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default FAQWrapperElements
-
-
+export default FAQWrapperElements;
