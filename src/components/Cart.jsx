4a075982bbbf36689c8e37c755f871a4bf3834f8
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-08-20 12:01:36 +0200

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,12 +10,20 @@ const Cart = () => {
     <div className="w-[300px] min-h-[32px] max-h-[440px] bg-[white] gap-2.5 flex flex-col items-center justify-start p-[15px] rounded-[20px] border-2 border-solid border-[#2A5B45]">
       <h2 className="text-[#2A5B45] text-xl leading-7 font-semibold">Cesta</h2>
       <div className="flex flex-col gap-2.5 overflow-y-auto scrollbar-hide">
-        {dataSelected.map((productSelected, i) => {
-          return <CardProductsCart key={i} product={productSelected} />;
-        })}
+        {dataSelected.length === 0 ? (
+          <p className="text-[rgba(0,0,0,0.4)]">El carrito está vacío.</p>
+        ) : (
+          dataSelected.map((productSelected, i) => {
+            return <CardProductsCart key={i} product={productSelected} />;
+          })
+        )}
       </div>
       <div className="flex items-center justify-between w-60">
-        <Buttons text={`Check-Out: ${totalPrice.toFixed(2)} €`} typeBtn={"cart"} link={"/bag"} />
+        <Buttons
+          text={`Check-Out: ${totalPrice.toFixed(2)} €`}
+          typeBtn={"cart"}
+          link={"/bag"}
+        />
         <Buttons text={"Clear"} typeBtn={"cart"} />
       </div>
     </div>
