ffe9747eaac67f1eb33d3a375c31c797a406e483
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-08-21 23:06:36 +0200

    finish formulario landing

--- src/context/FormInfoContext.jsx
+++ src/context/FormInfoContext.jsx
@@ -1,15 +1,25 @@
 "use client";
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const FormInfoContext = createContext(null);
 
-export default function FormInfoContextProvider({children}) {
+export default function FormInfoContextProvider({ children }) {
+  const [dataUsers, setDataUsers] = useState({});
 
-	const [userName, setUserName] = useState(null);
+  useEffect(() => {
+    const dataUserLs = JSON.parse(localStorage.getItem("dataUsers") || "[]");
+    setDataUsers(dataUserLs);
+  }, []);
 
-	return(
-		<FormInfoContext.Provider value={{userName, setUserName}}>
-			{children}
-		</FormInfoContext.Provider>
-	)
+  const updateUser = (user) => {
+    setDataUsers(user);
+    localStorage.setItem("dataUsers", JSON.stringify(user));
+  };
+  
+
+  return (
+    <FormInfoContext.Provider value={{ updateUser, dataUsers }}>
+      {children}
+    </FormInfoContext.Provider>
+  );
 }
