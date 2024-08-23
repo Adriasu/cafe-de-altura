ad539be807210a052e79cd257fa30f7d52eabaab
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-08-23 19:58:30 +0200

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,6 +7,9 @@ import CardProductsCart from "./CardProductsCart";
 const Cart = () => {
   const { dataSelected, totalPrice, btnClearCart } =
     useContext(ProductsContext);
+
+    const styleBtn =  dataSelected.length > 0 ? "cart" : "soldOut";
+
   return (
     <div className="w-[300px] min-h-[32px] max-h-[440px] bg-[white] gap-2.5 flex flex-col items-center justify-start p-[15px] rounded-[20px] border-2 border-solid border-[#2A5B45]">
       <h2 className="text-[#2A5B45] text-xl leading-7 font-semibold">Cesta</h2>
@@ -22,12 +25,15 @@ const Cart = () => {
       <div className="flex items-center justify-between w-60">
         <Buttons
           text={`Check-Out: ${totalPrice.toFixed(2)} €`}
-          typeBtn={"cart"}
-          link={"/bag"}
+          typeBtn={styleBtn}
+          link={dataSelected.length > 0 ? "/bag" : ""}
         />
-        <div onClick={() => btnClearCart(dataSelected)}>
-          <Buttons text={"Clear"} typeBtn={"cart"} />
-        </div>
+
+        <Buttons
+          text={"Clear"}
+          typeBtn={styleBtn}
+          onClick={() => btnClearCart(dataSelected)}
+        />
       </div>
     </div>
   );
