1194f2fdaa5e88dc34907c498ed20ba41cfd67be
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-08-27 19:02:09 +0200

    arreglado heigth bag

--- src/components/CopyRight.jsx
+++ src/components/CopyRight.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CopyRight = () => {
   return (
-    <div className="bg-[#2B2A2B] w-full h-10 flex items-center justify-center text-white text-sm font-semibold ">
+    <div className="bg-[#2B2A2B] w-full h-10 flex items-center justify-center text-white text-sm font-semibold table-fixed">
       <p>Todos los derechos reservados - Café de Altura SL - 2024</p>
     </div>
   );
